dea3cfddbf63b9f130a70c295b9eba8007703a65
Electricity NW <electricitynw@ipv6.local>
2017-10-25 11:21:46 +0100

    added camel core depedency

---
+++ src/main/java/com/learncamel/simpleroute/CopyFile.java
@@ -0,0 +1,13 @@
+package com.learncamel.simpleroute;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFile {
+
+    public static void main(String[] args) {
+
+        CamelContext camelContext = new DefaultCamelContext();
+
+    }
+}
